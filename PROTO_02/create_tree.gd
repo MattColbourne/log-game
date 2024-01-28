ccc47d9bd93513857dabec427db9eeedbe2c975b
Tyler Morgan <150198432+ttm0z@users.noreply.github.com>
2024-01-27 23:32:18 -0330

    bound checking

--- PROTO_02/create_tree.gd
+++ PROTO_02/create_tree.gd
@@ -14,7 +14,16 @@ func _process(delta):
 		var instance = tree.instantiate()
 		var rng = RandomNumberGenerator.new()
 		var x = rng.randi_range(0, screenSize.x)
+		if x < (screenSize.x / 2) + 50 and x > (screenSize.x / 2) - 50:
+			x -= 100
+		if x < 25: x += 50
+		if x > screenSize.x - 25: x -= 50
 		var y = rng.randi_range(0, screenSize.y)
+		if y < (screenSize.y / 2) + 50 and y > (screenSize.y / 2) - 50:
+			y += 100
+		if y < 25: y += 50
+		if y > screenSize.y - 25: y -= 50
+		
 		instance.position = Vector2(x,y)
 		add_child(instance)
 		num_trees += 1
