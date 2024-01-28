636c730dd49b93e07cebb47d42120a44afcabfa5
DylanWinter <dylanwinter11.2@gmail.com>
2024-01-28 00:12:22 -0330

    finished all base functionality!

---
+++ PROTO_02/Timer.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var scorePerSec: int
+@export var display: Label
+
+var score: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score = 0.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	score += scorePerSec*delta
+	display.text = str(score)
